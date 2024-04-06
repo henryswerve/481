a88d6f546f335b30329d67421da87972135ddde1
henryswerve <henrytran1243@gmail.com>
2024-04-05 18:23:00 -0700

    initial commit

---
+++ pset2.py
@@ -0,0 +1,37 @@
+# Henry Tran
+# ECON 481
+# Pset 2
+
+import numpy as np
+import scipy.optimize
+
+# exercise 0 
+
+def github() -> str:
+    """
+    some docstrings.
+    """
+
+    return None
+
+# exercise 1
+
+seed = 0
+
+def simulate_data(seed: int) -> tuple:
+    """
+    Some docstrings.
+    """
+
+    return None
+
+# exercise 2
+
+import numpy as np
+
+def estimate_mle(y: np.array, X: np.array) -> np.array:
+    """
+    Some docstrings.
+    """
+
+    return None
