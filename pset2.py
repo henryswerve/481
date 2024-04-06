7cc8a9d58d5bb6e3b34ca8902ea33bbc0d7df21f
henryswerve <henrytran1243@gmail.com>
2024-04-05 18:52:30 -0700

    problems down

--- pset2.py
+++ pset2.py
@@ -12,17 +12,20 @@ def github() -> str:
     some docstrings.
     """
 
-    return None
+    return "https://github.com/henryswerve/481/blob/main/pset2.py"
 
 # exercise 1
 
-seed = 0
+seed = 481
 
 def simulate_data(seed: int) -> tuple:
     """
     Some docstrings.
     """
 
+    # y is a 1000 x 1 array and x is a 1000 x 3 array
+    # y = np.array()
+    # x = np.array()
     return None
 
 # exercise 2
@@ -30,6 +33,16 @@ def simulate_data(seed: int) -> tuple:
 import numpy as np
 
 def estimate_mle(y: np.array, X: np.array) -> np.array:
+    """
+    Some docstrings.
+    """
+
+
+    return None
+
+# exercise 3
+
+def estimate_ols(y: np.array, X: np.array) -> np.array:
     """
     Some docstrings.
     """
