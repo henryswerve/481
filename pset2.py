75ff73a17eaba35a5cbcdbefd660011d8ce45c3e
henryswerve <henrytran1243@gmail.com>
2024-04-07 21:09:49 -0700

    fix newx_1, newx_2, ...

--- pset2.py
+++ pset2.py
@@ -25,67 +25,73 @@ def github() -> str:
 
 seed = 481
 
-x_1 = np.random.normal(0, 2, 1000)
-x_2 = np.random.normal(0, 2, 1000)
-x_3 = np.random.normal(0, 2, 1000)
-e_i = np.random.normal(0, 1)
-y = np.random.normal(0, 2, 1000)
+# x_1 = np.random.normal(0, 2, 1000)
+# x_2 = np.random.normal(0, 2, 1000)
+# x_3 = np.random.normal(0, 2, 1000)
+# e_i = np.random.normal(0, 1)
+# y = np.random.normal(0, 2, 1000)
 
-newx_1 = 3 * x_1
-newx_2 = 2 * x_2
-newx_3 = 6 * x_3
+# newx_1 = 3 * x_1
+# newx_2 = 2 * x_2
+# newx_3 = 6 * x_3
 
-y = np.array([y])
-y_T = np.transpose(y)
-print(y_T.shape)
+# y = np.array([y])
+# y_T = np.transpose(y)
+# print(y_T.shape)
 # print(y_T)
 # print(y)
-X = np.array([x_1, x_2, x_3])
 
-empty = (0 , 0)
+# X = np.array([x_1, x_2, x_3])
 
-np.asarray(empty)
+# empty = (0 , 0)
 
-print(empty)
+# np.asarray(empty)
+
+# print(empty)
+
 # fits 1000 x 3 per the problem
-X_T = np.transpose(X)
+
+# X_T = np.transpose(X)
 
 # print(X_T)
 
 # empty_tuple = np.asarray((y_T, X_T), dtype = tuple)
 
-empty_tuple = (y_T.flatten(), X_T)
+# empty_tuple = (y_T.flatten(), X_T)
 
-print(empty_tuple)
+# print(empty_tuple)
 
-print(len(empty_tuple))
+# print(len(empty_tuple))
 
 def simulate_data(seed: int) -> tuple:
     """
     Some docstrings.
     """
-    empty = (0 , 0)
-    x_1 = np.random.normal(0, 2, 1000)
-    x_2 = np.random.normal(0, 2, 1000)
-    x_3 = np.random.normal(0, 2, 1000)
-    e_i = np.random.normal(0, 1)
-    y = np.random.normal(0, 2, 1000)
+    seed = np.random.default_rng(seed = 481)
+    plus_5 = 5 * np.ones((1000, 1))
+    x_1 = seed.normal(0, 2, 1000)
+    x_2 = seed.normal(0, 2, 1000)
+    x_3 = seed.normal(0, 2, 1000)
+    e_i = seed.normal(0, 1)
+    y = seed.normal(0, 2, 1000)
 
-    newx_1 = 3 * x_1
-    newx_2 = 2 * x_2
-    newx_3 = 6 * x_3
-    X = np.array([x_1, x_2, x_3])
-    X_T = np.transpose(X)
+    newx_1 = 3 * x_1 + plus_5
+    newx_2 = 2 * x_2 + plus_5
+    newx_3 = 6 * x_3 + plus_5
+    X_tobe = np.array([x_1, x_2, x_3])
+    X = np.transpose(X_tobe)
 
-    y = np.array([y])
-    y_T = np.transpose(y)
+    y_tobe = np.array([y])
+    y = np.transpose(y_tobe)
 
-    y_and_x = (y_T.flatten(), X_T)
+    y_and_x = (y, X)
     return y_and_x
 
 print(simulate_data(481))
 
 # exercise 2
+
+# print(y_and_x)
 
 def estimate_mle(y: np.array, X: np.array) -> np.array:
     """
@@ -94,6 +100,8 @@ def estimate_mle(y: np.array, X: np.array) -> np.array:
 
 
     return None
+
+# print(estimate_mle())
 
 # exercise 3
 
