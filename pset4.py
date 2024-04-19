898e5ca1c1efc8c490ec7cd9e83c74e7e18af858
henryswerve <henrytran1243@gmail.com>
2024-04-18 21:11:34 -0700

    no. 2

--- pset4.py
+++ pset4.py
@@ -2,9 +2,39 @@
 # Pset 4
 # Econ 481
 
+# exercise 0 
 def github() -> str:
     """
     Some docstrings.
     """
 
-    return "https://github.com/<user>/<repo>/blob/main/<filename.py>"
+    return "https://github.com/henryswerve/481/blob/main/pset4.py"
+
+# exercise 1
+
+import pandas as pd
+
+def load_data() -> pd.DataFrame:
+    """
+    Some docstrings.
+    """
+    directory = f'https://lukashager.netlify.app/econ-481/data/TSLA.csv'
+    data = pd.read_csv(directory)
+
+    return data
+
+df = load_data()
+
+# exercise 2
+
+import matplotlib.pyplot as plt
+
+def plot_close(df: pd.DataFrame, start: str = '2010-06-29', end: str = '2024-04-15') -> None:
+    """
+    Some docstrings
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot()
+
+    # ax.plot(df, color = "black",
+    #         linestyle = "dashed", marker = "o")
