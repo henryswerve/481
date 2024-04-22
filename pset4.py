e7130faddcb088af1326371588b44e98e4b4008b
henryswerve <henrytran1243@gmail.com>
2024-04-21 21:38:49 -0700

    idk

--- pset4.py
+++ pset4.py
@@ -19,11 +19,15 @@ def load_data() -> pd.DataFrame:
     Some docstrings.
     """
     directory = f'https://lukashager.netlify.app/econ-481/data/TSLA.csv'
-    data = pd.read_csv(directory)
+    data = pd.read_csv(directory,
+                    #    index_col = 0,
+                       parse_dates = True)
 
     return data
 
 df = load_data()
+
+print(list(df.columns))
 
 # exercise 2
 # Please write a function called plot_close which takes the output of load_data() as defined above, as well as an optional start and end date 
@@ -31,31 +35,36 @@ df = load_data()
 # Please include the date range in the title of the graph. Note that this function needn’t return anything, just plot a graph using matplotlib
 
 import matplotlib.pyplot as plt
+# df.columns()
 
-# print(df)
+# fig = plt.figure()
+# ax = fig.add_subplot()
+# graph = ax.plot(df['Close'], color = 'black',
+#                 linestyle = 'dashed', marker = 'o')
 
-start = '2010-06-29'
-end = '2024-04-15'
-data = df.reset_index()
-start_date = list(df['Date'])
-close_price = list(df['Close'])
-
-plt.plot(start_date, close_price)
+# dates = df['Date']
+# closing = df['Close']
+# plt.plot(dates, closing)
 
 
+# plt.show()
 
 def plot_close(df: pd.DataFrame, start: str = '2010-06-29', end: str = '2024-04-15') -> None:
     """
     Some docstrings
     """
-    start = '2010-06-29'
-    end = '2024-04-15'
-    data = df.reset_index()
-    start_date = list(df['Date'])
-    close_price = list(df['Close'])
-
-    closed_plot = plt.plot(start_date, close_price)
     
-    return closed_plot
+    df = df.loc[(df['Date'] >= start) & (df['Date'] <= end)]
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    graph = ax.plot(df['Close'], color = 'black',
+                    linestyle = '-', marker = '.')
 
-print(plot_close(df, '2010-06-29', '2024-04-15'))
+    dates = df['Date']
+    closing = df['Close']
+    plt.plot(dates, closing)
+
+    showing = plt.show()
+    return showing
+
+# print(plot_close(df, '2010-06-29', '2024-04-15'))
