928f668b6c452dec5db41b038e494946b53fe489
henryswerve <henrytran1243@gmail.com>
2024-05-04 20:13:29 -0700

    progress

--- pset5.py
+++ pset5.py
@@ -6,6 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import regex as re
+import time
 
 def github() -> str:
     """
@@ -14,30 +15,38 @@ def github() -> str:
 
     return "https://github.com/<user>/<repo>/blob/main/<filename.py>"
 
+# assign link to link
 link = "https://lukashager.netlify.app/teaching/#ECON-481"
 
+# test the link
 r = requests.get(link)
 assert r.ok
 
+# initiailze beautiful soup
 soup = BeautifulSoup(r.text, features="lxml")
 
+# find all <a> elements from link
 links = soup.find_all('a')
-# url_pattern = re.compile("https://lukashager.netlify.app/econ-481/00_intro_to_481#/title-slide")
 
-url_pattern = re.compile("https://lukashager.netlify.app/econ-481/\d{2}_\[a-z]#/title-slide")
+# use regex to filter by digit #, and file extension
+match = re.compile('\d{2}[a-z_]+[^.]+$')
 
+# filters hrefs based on match criteria
+links = [x['href'] for x in soup.find_all('a', {'href': match})]
 
-# match = url_pattern.match(link)
+# list comprehension to remove the last terms in the list
+# is there a better way to do this?
+urls = [x for i, x in enumerate(links) if i < 7]
 
-# if match:
-#     print("matched")
-# else:
-#     print("no")
+# print(urls)
 
-
-links = [x['href'] for x in soup.find_all('a', {'href': url_pattern})]
-print(links[:5])
-
+# for each url, get the text
+for url in urls:
+    url = f'https://lukashager.netlify.app' + url
+    r1 = requests.get(url)
+    assert r1.ok
+    r1_bs = BeautifulSoup(r1.text, features = 'lxml')
+    time.sleep(3)
 
 
 def scrape_code(url: str) -> str:
