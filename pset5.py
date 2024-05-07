cb8acdb754e19a385828f751c008fdf881fbc179
henryswerve <henrytran1243@gmail.com>
2024-05-06 17:15:42 -0700

    progress

--- pset5.py
+++ pset5.py
@@ -25,8 +25,9 @@ assert r.ok
 # initiailze beautiful soup
 soup = BeautifulSoup(r.text, features="lxml")
 
-# find all <a> elements from link
-links = soup.find_all('a')
+# # find all <a> elements from link
+# dont think we need this
+# links = soup.find_all('a')
 
 # use regex to filter by digit #, and file extension
 match = re.compile('\d{2}[a-z_]+[^.]+$')
@@ -36,17 +37,43 @@ links = [x['href'] for x in soup.find_all('a', {'href': match})]
 
 # list comprehension to remove the last terms in the list
 # is there a better way to do this?
-urls = [x for i, x in enumerate(links) if i < 7]
+tobe_urls = [x for i, x in enumerate(links) if i < 7]
 
 # print(urls)
 
-# for each url, get the text
-for url in urls:
-    url = f'https://lukashager.netlify.app' + url
-    r1 = requests.get(url)
+# for each url, get the links and create urls_list
+# and obtain the html text using BeautifulSoup
+urls_list = []
+for url in tobe_urls:
+    tobe_list = f'https://lukashager.netlify.app{url}'
+    r1 = requests.get(tobe_list)
     assert r1.ok
     r1_bs = BeautifulSoup(r1.text, features = 'lxml')
+    urls_list.append(tobe_list)
     time.sleep(3)
+
+# print(urls_list)
+
+# test = "https://lukashager.netlify.app/econ-481/05_web_scraping"
+# r_test = requests.get(test)
+# assert r_test.ok
+# testbs = BeautifulSoup(r_test.text, features = 'lxml')
+# search_criteria = ["sourceCode"]
+# test_soup = testbs.body.find_all("div", search_criteria) # does this get all of our code??? or is there more?
+tobe_string = ""
+for urls in urls_list:
+    r_test = requests.get(urls)
+    assert r_test.ok
+    testbs = BeautifulSoup(r_test.text, features = 'lxml')
+    search_criteria = ["sourceCode"]
+    test_soup = testbs.body.find_all("div", search_criteria) # does this get all of our code??? or is there more?
+    tobe_string.join(test_soup) # how do i add all the strings together?
+    # rationale: get all into one string, then omit certain words until code shows through
+    # convoluted method. there is def. a better way
+    time.sleep(3)
+
+# print(tobe_string.head())
+
 
 
 def scrape_code(url: str) -> str:
